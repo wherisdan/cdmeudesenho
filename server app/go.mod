9da415dd24f4a4c2f0f4500c195ffb48164ab36b
wherisdan <danielf170704@gmail.com>
2024-07-04 17:38:32 -0300

    first commit

---
+++ server app/go.mod
@@ -0,0 +1,3 @@
+module github.com/wherisdan/cdmeudesenho/server
+
+go 1.21.6
---
+++ server app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// r := gin.Default()
+
+	// r.LoadHTMLGlob("./dist/index.html")
+	// r.Static("/assets", "./dist/assets")
+
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.html", nil)
+	// })
+
+	// r.Run(":8080")
+	fmt.Println("ola docker")
+}
