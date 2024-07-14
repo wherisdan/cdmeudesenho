1f893720c164b02e92ba4f3885b7d1230993ab2a
wherisdan <danielf170704@gmail.com>
2024-07-13 23:42:17 -0300

    example page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+function comprar() {
+  console.log("o cliente quer comprar")
+}
 </script>
 
 <template>
-  <h1>
-    ola mundo
-  </h1>
+  <h1>Bem-vindo ao cadê meu desenho</h1>
+  <h2>Aqui, a imaginação não tem limites!</h2>
+  <button @click="comprar">comprar agora</button>
 </template>
 
 <style></style>
